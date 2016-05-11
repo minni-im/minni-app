ef3d07bfe514b30777d774977fb35afc2ef85269
Benoit Charbonnier <benoit.charbonnier@gmail.com>
2016-05-11 16:29:26 +0200

    fix: async dispatching in ComposerAction.saveCurrentText because in some cases we already be inside a dispatch when enterring it

--- app-frontend/actions/ComposerActionCreators.js
+++ app-frontend/actions/ComposerActionCreators.js
@@ -1,9 +1,9 @@
-import { dispatch } from "../Dispatcher";
+import { dispatchAsync } from "../Dispatcher";
 import { ActionTypes } from "../Constants";
 
 export default {
   saveCurrentText(roomId, text) {
-    dispatch({
+    dispatchAsync({
       type: ActionTypes.COMPOSER_TEXT_SAVE,
       roomId,
       text
