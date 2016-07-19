90af54c8470eab512155a594c7c870337eb10045
Benoit Charbonnier <benoit.charbonnier@gmail.com>
2016-07-19 16:48:20 +0200

    feat: Message pre-encoding (#6)
    * wip: mention working, room almost
    
    * feat: room & mention detected and encoded/decoded

--- app-frontend/utils/MarkupUtils.js
+++ app-frontend/utils/MarkupUtils.js
@@ -1,188 +1,202 @@
 import React from "react"; // needed for the jsx below.
 import SimpleMarkdown from "simple-markdown";
 import highlight from "highlight.js";
+import { Link } from "react-router";
+
 import Emoji from "../components/Emoji.react";
+
+import UserStore from "../stores/UserStore";
+import RoomStore from "../stores/RoomStore";
+import SelectedAccountStore from "../stores/SelectedAccountStore";
 
 const DEFAULT_LINK_RULE = SimpleMarkdown.defaultRules.link;
 
-function createRules() {
-  return {
-    newline: SimpleMarkdown.defaultRules.newline,
-    paragraph: SimpleMarkdown.defaultRules.paragraph,
-    escape: SimpleMarkdown.defaultRules.escape,
-    link: {
-      ...DEFAULT_LINK_RULE,
-      match() {
-        return null;
-      },
-      react(node, output, state) {
-        return (
-          <a
-            key={state.key}
-            href={SimpleMarkdown.sanitizeUrl(node.target)}
-            title={node.title}
-            target="_blank"
-          >{output(node.content, state)}</a>
-        );
-      }
+const DEFAULT_RULES = {
+  newline: SimpleMarkdown.defaultRules.newline,
+  paragraph: SimpleMarkdown.defaultRules.paragraph,
+  escape: SimpleMarkdown.defaultRules.escape,
+  link: {
+    ...DEFAULT_LINK_RULE,
+    match() {
+      return null;
     },
-    autolink: SimpleMarkdown.defaultRules.autolink,
-    url: SimpleMarkdown.defaultRules.url,
-    strong: SimpleMarkdown.defaultRules.strong,
-    em: SimpleMarkdown.defaultRules.em,
-    u: SimpleMarkdown.defaultRules.u,
-    br: SimpleMarkdown.defaultRules.br,
-    text: SimpleMarkdown.defaultRules.text,
-    inlineCode: SimpleMarkdown.defaultRules.inlineCode,
-
-    s: {
-      order: SimpleMarkdown.defaultRules.u.order,
-      match: SimpleMarkdown.inlineRegex(/^~([\s\S]+?)~(?!_)/),
-      parse: SimpleMarkdown.defaultRules.u.parse,
-      react(node, output, state) {
-        return (
-          <s key={state.key}>{output(node.content, state)}</s>
-        );
+    react(node, output, state) {
+      return (
+        <a
+          key={state.key}
+          href={SimpleMarkdown.sanitizeUrl(node.target)}
+          title={node.title}
+          target="_blank"
+        >{output(node.content, state)}</a>
+      );
+    }
+  },
+  autolink: SimpleMarkdown.defaultRules.autolink,
+  url: SimpleMarkdown.defaultRules.url,
+  strong: SimpleMarkdown.defaultRules.strong,
+  em: SimpleMarkdown.defaultRules.em,
+  u: SimpleMarkdown.defaultRules.u,
+  br: SimpleMarkdown.defaultRules.br,
+  text: SimpleMarkdown.defaultRules.text,
+  inlineCode: SimpleMarkdown.defaultRules.inlineCode,
+  s: {
+    order: SimpleMarkdown.defaultRules.u.order,
+    match: SimpleMarkdown.inlineRegex(/^~([\s\S]+?)~(?!_)/),
+    parse: SimpleMarkdown.defaultRules.u.parse,
+    react(node, output, state) {
+      return (
+        <s key={state.key}>{output(node.content, state)}</s>
+      );
+    }
+  },
+  codeBlock: {
+    order: SimpleMarkdown.defaultRules.codeBlock.order,
+    match(source, state, lookBehind) {
+      const match = /^(`{3,})(?:([A-z0-9\-]+?)\n+)?\n*?([^]+?)\1/.exec(source);
+      // We only detect code block (or also known as code fence)
+      // that compose the entire message. If text is detected before,
+      // we let the inlineCode matcher do the job
+      if (match && lookBehind.trim() === "") {
+        return match;
       }
+      return null;
     },
 
-    codeBlock: {
-      order: SimpleMarkdown.defaultRules.codeBlock.order,
-      match(source, state, lookBehind) {
-        const match = /^(`{3,})(?:([A-z0-9\-]+?)\n+)?\n*?([^]+?)\1/.exec(source);
-        // We only detect code block (or also known as code fence)
-        // that compose the entire message. If text is detected before,
-        // we let the inlineCode matcher do the job
-        if (match && lookBehind.trim() === "") {
-          return match;
-        }
-        return null;
-      },
+    parse(capture) {
+      return {
+        lang: (capture[2] || "").trim(),
+        content: (capture[3] || "").trim()
+      };
+    },
 
-      parse(capture) {
-        return {
-          lang: (capture[2] || "").trim(),
-          content: (capture[3] || "").trim()
-        };
-      },
-
-      react(node, output, state) {
-        if (node.lang && highlight.getLanguage(node.lang) !== null) {
-          const code = highlight.highlight(node.lang, node.content);
-          return (
-            <div
-              className="message--code-block"
-              key={state.key}
-            >
-              <div className="language">{`</> ${code.language}`}</div>
-              <pre>
-                <code
-                  className={`hljs ${code.language}`}
-                  dangerouslySetInnerHTML={{ __html: code.value }}
-                ></code>
-              </pre>
-            </div>
-          );
-        }
-        const code = highlight.highlightAuto(node.content);
+    react(node, output, state) {
+      if (node.lang && highlight.getLanguage(node.lang) !== null) {
+        const code = highlight.highlight(node.lang, node.content);
         return (
           <div
             className="message--code-block"
             key={state.key}
           >
-            <div className="language">{`<?> Is it some '${code.language}' ?`}</div>
+            <div className="language">{`</> ${code.language}`}</div>
             <pre>
-              <code className="hljs">{node.content}</code>
+              <code
+                className={`hljs ${code.language}`}
+                dangerouslySetInnerHTML={{ __html: code.value }}
+              ></code>
             </pre>
           </div>
         );
       }
-    },
-
-    blockQuote: SimpleMarkdown.defaultRules.blockQuote,
-
-    emoji: {
-      order: SimpleMarkdown.defaultRules.text.order,
-      match(source) {
-        return /^:([a-zA-Z0-9-_+]+):(:skin-tone-((?:1\-2|[3-6])):)?/.exec(source);
-      },
-      parse(capture) {
-        const name = capture[1];
-        return { name, skinTone: capture[2] && capture[3] };
-      },
-      react(node, output, state) {
-        return <Emoji key={state.key} shortname={node.name} skinTone={node.skinTone} />;
-      }
-    },
-
-    mention: {
-      order: SimpleMarkdown.defaultRules.text.order,
-      match(source, state, lookBehind) {
-        if (/^|[^a-zA-Z0-9_!#$%&*@＠]/.test(lookBehind)) {
-          return /^[@＠]([a-zA-Z0-9_]{1,20})/.exec(source);
-        }
-        return null;
-      },
-
-      parse(capture) {
-        return {
-          name: capture[1]
-        };
-      },
-
-      react(node, output, state) {
-        return (
-          <b className="mention" key={state.key}>@{node.name}</b>
-        );
-      }
-    },
-
-    room: {
-      order: SimpleMarkdown.defaultRules.text.order,
-      match(source) {
-        return /^<#(\d+)>/.exec(source);
-      },
-      parse(capture) {
-
-      },
-
-      handleClick(e) {
-
-      },
-
-      react(node, output, state) {
-        return (
-          <span
-            key={state.key}
-            onClick={this.handleClick}
-          />
-        )
-      }
-    },
-
-    hashtag: {
-      order: SimpleMarkdown.defaultRules.text.order,
-      match(source, state, lookBehind) {
-        if (/^|[^a-zA-Z0-9_!#$%&*@＠]/.test(lookBehind)) {
-          return /^#([a-zA-Z0-9_-]{1,20})/.exec(source);
-        }
-        return null;
-      },
-      parse(capture) {
-        return {
-          content: capture[1]
-        };
-      },
-      react(node, output, state) {
-        return (
-          <b key={state.key} className="hashtag">#{node.content}</b>
-        );
-      }
+      const code = highlight.highlightAuto(node.content);
+      return (
+        <div
+          className="message--code-block"
+          key={state.key}
+        >
+          <div className="language">{`<?> Is it some '${code.language}' ?`}</div>
+          <pre>
+            <code className="hljs">{node.content}</code>
+          </pre>
+        </div>
+      );
     }
-  };
+  },
+
+  blockQuote: SimpleMarkdown.defaultRules.blockQuote,
+
+  emoji: {
+    order: SimpleMarkdown.defaultRules.text.order,
+    match(source) {
+      return /^:([a-zA-Z0-9-_+]+):(:skin-tone-((?:1\-2|[3-6])):)?/.exec(source);
+    },
+    parse(capture) {
+      const name = capture[1];
+      return { name, skinTone: capture[2] && capture[3] };
+    },
+    react(node, output, state) {
+      return <Emoji key={state.key} shortname={node.name} skinTone={node.skinTone} />;
+    }
+  },
+
+  mention: {
+    order: SimpleMarkdown.defaultRules.text.order,
+    match(source) {
+      return /^<@([0-9a-z]{32})>/.exec(source);
+    },
+    parse(capture) {
+      const user = UserStore.getUser(capture[1]);
+      return {
+        userId: user !== null ? user.id : null,
+        content: [{
+          type: "text",
+          content: user !== null ? `@${user.nickname}` : capture[0]
+        }]
+      };
+    },
+    react(node, output, state) {
+      return (
+        <b
+          className="mention"
+          key={state.key}
+        >{output(node.content, state)}</b>
+      );
+    }
+  },
+
+  room: {
+    order: SimpleMarkdown.defaultRules.text.order,
+    match(source) {
+      return /^<#([0-9a-z-]{32})>/.exec(source);
+    },
+    parse(capture) {
+      const accountSlug = SelectedAccountStore.getAccount().slug;
+      const room = RoomStore.get(capture[1]);
+      return {
+        accountSlug,
+        roomSlug: room.slug,
+        content: [{
+          type: "text",
+          content: room !== null ? `#${room.slug}` : capture[0]
+        }]
+      };
+    },
+    react(node, output, state) {
+      return (
+        <Link
+          key={state.key}
+          className="hashtag hashtag--room"
+          to={`/chat/${node.accountSlug}/messages/${node.roomSlug}`}
+        >{output(node.content, state)}</Link>
+      );
+    }
+  },
+
+  hashtag: {
+    order: SimpleMarkdown.defaultRules.text.order,
+    match(source, state, lookBehind) {
+      if (/^|[^a-zA-Z0-9_!#$%&*@＠]/.test(lookBehind)) {
+        return /^#([a-zA-Z0-9_-]{1,20})/.exec(source);
+      }
+      return null;
+    },
+    parse(capture) {
+      return {
+        content: capture[1]
+      };
+    },
+    react(node, output, state) {
+      return (
+        <b key={state.key} className="hashtag">#{node.content}</b>
+      );
+    }
+  }
+};
+
+export function getDefaultRules() {
+  return { ...DEFAULT_RULES };
 }
 
-function createContentParser(rules, content = "", inline= true, state = {}) {
+function createContentParser(rules, content = "", inline = true, state = {}) {
   const parser = SimpleMarkdown.parserFor(rules);
   const output = SimpleMarkdown.reactFor(SimpleMarkdown.ruleOutput(rules, "react"));
 
@@ -194,11 +208,11 @@ function createContentParser(rules, content = "", inline= true, state = {}) {
 }
 
 export function parseContent(content, inline, state) {
-  return createContentParser(createRules(), content, inline, state);
+  return createContentParser(getDefaultRules(), content, inline, state);
 }
 
 export function parseTitle(content, withLink = true) {
-  const defaultRules = createRules();
+  const defaultRules = getDefaultRules();
   let rules = {
     emoji: defaultRules.emoji,
     text: defaultRules.text
--- app-frontend/utils/MessageUtils.js
+++ app-frontend/utils/MessageUtils.js
@@ -1,10 +1,15 @@
 import Long from "long";
+import SimpleMarkdown from "simple-markdown";
 
 import { PLUGIN_TYPES } from "../Constants";
 
+import AccountRoomStore from "../stores/AccountRoomStore";
 import PluginsStore from "../stores/PluginsStore";
+import RoomStore from "../stores/RoomStore";
 import UserStore from "../stores/UserStore";
 import SelectedAccountStore from "../stores/SelectedAccountStore";
+
+import * as MarkupUtils from "../utils/MarkupUtils";
 
 function createNonce() {
   return Long.fromNumber(new Date().getTime())
@@ -13,14 +18,139 @@ function createNonce() {
     .shiftLeft(22);
 }
 
-export function createMessage(roomId, content) {
+const DEFAULT_TEXT_RULE = SimpleMarkdown.defaultRules.text;
+const DEFAULT_RULES = MarkupUtils.getDefaultRules();
+
+function matchAsText(simpleRule) {
+  return {
+    order: simpleRule.order,
+    match: simpleRule.match,
+    parse(capture) {
+      return {
+        type: "text",
+        content: capture[0]
+      };
+    },
+    text(node) {
+      return node.content;
+    }
+  };
+}
+
+const ENCODING_RULES = {
+  // We need to lookup for links otherwise we could get mentions and rooms from
+  // within them :)
+  link: matchAsText(SimpleMarkdown.defaultRules.link),
+  url: matchAsText(SimpleMarkdown.defaultRules.url),
+  room: {
+    order: DEFAULT_RULES.room.order,
+    match(source, state, lookBehind) {
+      if (/^|[\s]+/.test(lookBehind)) {
+        const match = source.match(/^#([a-z0-9-]{1,20})/);
+        if (match) {
+          return state.rooms
+            .filter(({ slug }) => slug === match[1])
+            .map(({ id }) => [match[0], id])
+            [0];
+        }
+      }
+      return null;
+    },
+    parse(capture) {
+      return {
+        type: "room",
+        content: `<#${capture[1]}>`
+      };
+    },
+    text(node) {
+      return node.content;
+    }
+  },
+  mention: {
+    order: DEFAULT_RULES.mention.order,
+    match(source, state, lookBehind) {
+      if (/^|[^a-zA-Z0-9_!#$%&*@＠]/.test(lookBehind)) {
+        const match = source.match(/^[@＠]([a-zA-Z0-9_]{1,20})/);
+        if (match) {
+          return state.users
+            .filter(({ nickname }) => nickname === match[1])
+            .map(({ id }) => [match[0], id])
+            [0];
+        }
+      }
+      return null;
+    },
+    parse(capture) {
+      return {
+        type: "mention",
+        content: `<@${capture[1]}>`
+      };
+    },
+    text(node) {
+      return node.content;
+    }
+  },
+  text: {
+    ...DEFAULT_TEXT_RULE,
+    text(node) {
+      return node.content;
+    }
+  }
+};
+
+// Generate proper rule's order index based
+Object.keys(ENCODING_RULES).forEach((type, i) => {
+  ENCODING_RULES[type].order = i;
+});
+
+// Transforming @mention and #link-room into proper object we can recognize
+export function encode(text) {
+  text = text.trim();
+  const account = SelectedAccountStore.getAccount();
+
+  const users = UserStore.getUsers(account.usersId)
+    .toArray()
+    .map(user => ({ id: user.id, nickname: user.nickname }));
+
+  const rooms = AccountRoomStore.getRooms(account.id)
+    .toArray()
+    .map(room => ({ id: room.id, slug: room.slug }));
+
+  const state = {
+    inline: true,
+    meta: {},
+    account,
+    users,
+    rooms
+  };
+
+  const parser = SimpleMarkdown.parserFor(ENCODING_RULES);
+  const textFor = SimpleMarkdown.htmlFor(SimpleMarkdown.ruleOutput(ENCODING_RULES, "text"));
+  const parsed = {
+    content: textFor(parser(text, state))
+  };
+  return parsed;
+}
+
+// Used when editing a message to revert private objects to @mention and #room
+export function decode(text) {
+  return text.replace(/<@([a-zA-Z0-9-]{32})>/g, (match, userId) => {
+    const user = UserStore.getUser(userId);
+    return user == null ? match : `@${user.nickname}`;
+  }).replace(/<#([a-zA-Z0-9-]{32})>/g, (match, roomId) => {
+    const room = RoomStore.get(roomId);
+    return room == null ? match : `#${room.slug}`;
+  });
+}
+
+export function createMessage(roomId, text) {
   const composerPlugins = PluginsStore.getPlugins(PLUGIN_TYPES.COMPOSER_TEXT);
   const preProcessors = composerPlugins.map(plugin => plugin.encodeMessage);
 
   const message = {
     id: createNonce().toString(),
     roomId,
-    content,
+    content: encode(text).content,
     type: "chat",
     accountId: SelectedAccountStore.getAccount().id,
     userId: UserStore.getConnectedUser().id
