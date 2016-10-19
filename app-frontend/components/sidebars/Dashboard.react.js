53e331b59afcb66758c28303e8cfb23843520134
Benoit Charbonnier <benoit.charbonnier@gmail.com>
2016-10-19 22:38:42 +0200

    fix: welcome screen with no sidebar

--- app-frontend/components/sidebars/Dashboard.react.js
+++ app-frontend/components/sidebars/Dashboard.react.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { Container } from "flux/utils";
 import { Link } from "react-router";
 
-const DashboardSidebar = () => (
-  <header className="welcome">
-    <h1>{Minni.name}</h1>
-    <h2>Welcome to {Minni.name}</h2>
-    <br />
-    <Link to="/create" className="button button-primary">Create new team</Link>
-  </header>
-);
+import AccountStore from "../../stores/AccountStore";
 
-export default DashboardSidebar;
+class DashboardSidebar extends React.Component {
+  static getStores() {
+    return [AccountStore];
+  }
+
+  static calculateState() {
+    return {
+      render: !AccountStore.getAccounts().isEmpty()
+    };
+  }
+
+  render() {
+    return this.state.render && (
+      <header className="welcome">
+        <h1>{Minni.name}</h1>
+        <h2>Welcome to {Minni.name}</h2>
+        <br />
+        <Link to="/create" className="button button-primary">Create new team</Link>
+      </header>
+    );
+  }
+}
+
+export default Container.create(DashboardSidebar);
