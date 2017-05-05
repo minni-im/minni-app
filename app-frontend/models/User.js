13aeccc983588e6166e9bb1655e1e77e9d406217
Benoit Charbonnier <benoit.charbonnier@gmail.com>
2017-05-05 15:29:00 +0200

    fix: user without firstname nor lastname are crashing initials generation code

--- app-frontend/models/User.js
+++ app-frontend/models/User.js
@@ -11,11 +11,14 @@ const UserRecord = Immutable.Record({
   email: undefined,
   gravatarEmail: undefined,
   picture: "/images/1x1.gif",
-  status: USER_STATUS.OFFLINE
+  status: USER_STATUS.OFFLINE,
 });
 
 export default class User extends UserRecord {
   get initials() {
-    return (this.firstname[0] + this.lastname[0]).toUpperCase();
+    if (this.firstname.length && this.lastname.length) {
+      return (this.firstname[0] + this.lastname[0]).toUpperCase();
+    }
+    return this.nickname[0].toUpperCase();
   }
 }
