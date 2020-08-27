33b7fb19ee00975c686bcc956f05e1aed5364b84
Benoit Charbonnier <benoit.charbonnier@gmail.com>
2020-08-27 21:48:20 +0200

    feat: moving to lessc + adding basic mobile support

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,20 @@
 const gulp = require("gulp");
 const gutil = require("gulp-util");
 const plumber = require("gulp-plumber");
-const less = require("gulp-less");
 const runSeq = require("run-sequence");
-const Autoprefixer = require("less-plugin-autoprefix");
-const CleanCss = require("less-plugin-clean-css");
 
 const nodeEnv = process.env.NODE_ENV || "development";
 const RELEASE = nodeEnv === "production";
 
-const STATIC_PATHS = ["./public/**/*", "!./public/images/svgs/*.js", "./assets/favicons/*"];
-
-const lessPlugins = [
-  new Autoprefixer({
-    browsers: ["last 2 versions"],
-  }),
+const STATIC_PATHS = [
+  "./public/**/*",
+  "!./public/images/svgs/*.js",
+  "./assets/favicons/*",
 ];
-if (RELEASE) {
-  lessPlugins.push(
-    new CleanCss({
-      advanced: true,
-      aggressiveMerging: true,
-    })
-  );
-}
 
-gulp.task("less", () => {
-  gulp
-    .src(["./assets/stylesheets/style.less", "./assets/stylesheets/chat.less"])
-    .pipe(
-      plumber({
-        errorHandler(error) {
-          gutil.log(
-            gutil.colors.cyan("Plumber") + gutil.colors.red(" found unhandled error:\n"),
-            error.toString()
-          );
-          this.emit("end");
-        },
-      })
-    )
-    .pipe(
-      less({
-        plugins: lessPlugins,
-      })
-    )
-    .pipe(plumber.stop())
-    .pipe(gulp.dest("./dist/public/css"));
-});
-
-gulp.task("static", () => gulp.src(STATIC_PATHS).pipe(gulp.dest("./dist/public")));
+gulp.task("static", () =>
+  gulp.src(STATIC_PATHS).pipe(gulp.dest("./dist/public"))
+);
 
 gulp.task("watch", () => {
   gulp.watch(STATIC_PATHS, ["static"]);
