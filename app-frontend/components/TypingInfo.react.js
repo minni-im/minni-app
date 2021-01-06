d033e30c329c0d5b44566d892fcfaa24b2743619
Benoit Charbonnier <benoit.charbonnier@gmail.com>
2021-01-06 22:57:30 +0100

    fix: TypingInfo broken reduce, fixing BSOD (Black Screen Of Death)

--- app-frontend/components/TypingInfo.react.js
+++ app-frontend/components/TypingInfo.react.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Container } from "flux/utils";
 
-import Avatar from "./generic/Avatar.react";
-
 import UserStore from "../stores/UserStore";
 import TypingStore from "../stores/TypingStore";
 
@@ -10,47 +8,61 @@ class TypingInfo extends React.Component {
   render() {
     const users = this.props.users;
     const viewer = UserStore.getConnectedUser();
-    const list = users.keySeq()
-      .filter(userId => userId !== viewer.id)
-      .map(userId => UserStore.getUser(userId))
-      .map(user => user.fullname)
+    const list = users
+      .keySeq()
+      .filter((userId) => userId !== viewer.id)
+      .slice(0, 4)
+      .map((userId) => UserStore.getUser(userId))
+      .map((user) => user.fullname)
       .toArray();
 
     let typingStream;
     switch (list.length) {
       case 1:
-        typingStream = <span><strong>{list[0]}</strong> is typing...</span>;
+        typingStream = (
+          <span>
+            <strong>{list[0]}</strong> is typing...
+          </span>
+        );
         break;
       case 2:
-        typingStream = <span><strong>{list[0]}</strong> and <strong>{list[1]}</strong> are typing...</span>;
+        typingStream = (
+          <span>
+            <strong>{list[0]}</strong> and <strong>{list[1]}</strong> are
+            typing...
+          </span>
+        );
         break;
       default:
         typingStream = list.reduce((result, userName, i) => {
           if (i === 0) {
-            return result.push(<strong key={i}>{userName}</strong>);
+            return result.concat(<strong key={String(i)}>{userName}</strong>);
           } else if (i === list.length - 1) {
-            return result.push(" and ", <strong key={i}>{userName}</strong>, " are typing...");
-          } else {
-            return result.push(", ", <strong key={i}>{userName}</strong>);
+            return result.concat(
+              " and ",
+              <strong key={String(i)}>{userName}</strong>,
+              " are typing..."
+            );
           }
+          return result.concat(
+            ", ",
+            <strong key={String(i)}>{userName}</strong>
+          );
         }, []);
     }
-    return <div className="typing-info flex-spacer">
-      {typingStream}
-    </div>;
+    return <div className="typing-info flex-spacer">{typingStream}</div>;
   }
 }
 
-
 class TypingInfoContainer extends React.Component {
   static getStores() {
-    return [ TypingStore ];
+    return [TypingStore];
   }
 
   static calculateState(prevProps, nextProps) {
     return {
       viewer: UserStore.getConnectedUser(),
-      users: TypingStore.getTypingUsers(nextProps.room.id)
+      users: TypingStore.getTypingUsers(nextProps.room.id),
     };
   }
 
